f9240512585ced65c83e4688062d27e42951e79d
DUO XU <mikechan2333@gmail.com>
2022-02-19 00:39:08 +0900

    変換の絶対誤差を測定した。

--- errortest.cxx
+++ errortest.cxx
@@ -14,8 +14,39 @@ int main(){
     for(int i=0;i<5;i++){
         float v=acclist[i];
         FixedPoint<uint32,4> v2=FixedPoint<uint32,4>::fromFloat(v),ans(bins[i]);
-        cout<<v<<" "<<v2<<" "<<ans<<endl;
-        cout<<"abserr="<<abs(v-v2.toDouble())<<endl;
+        cout<<v<<" "<<v2<<endl;
+        double diff=abs(v-v2.toDouble());
+        if (diff)
+            cerr<<"abserr="<<diff<<endl;
+    }
+
+    cout<<"------------"<<endl;
+
+    // 二進数で表し切れない小数
+    float errlist[5]={0.1f,1.1f,1.7f,4.6f,5.05f};
+    for(int i=0;i<5;i++){
+        float v=errlist[i];
+        FixedPoint<uint32,4> v2=FixedPoint<uint32,4>::fromFloat(v);
+        cout<<v<<" "<<v2<<endl;
+        const double eps=1./(1<<4);
+        double diff=abs(v-v2.toDouble());
+
+        if (diff>=eps)
+            cerr<<"abserr="<<abs(v-v2.toDouble())<<endl;
+    }
+
+    cout<<"------------"<<endl;
+
+    // 二進数で表し切れない小数
+    for(int i=0;i<5;i++){
+        float v=errlist[i];
+        FixedPoint<uint32,8> v2=FixedPoint<uint32,8>::fromFloat(v);
+        cout<<v<<" "<<v2<<endl;
+        const double eps=1./(1<<8);
+        double diff=abs(v-v2.toDouble());
+
+        if (diff>=eps)
+            cerr<<"abserr="<<abs(v-v2.toDouble())<<endl;
     }
 
     return 0;
